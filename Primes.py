745f04e3c9fdab399c5542b4c372cc8dd2bbbf9b
N0R4H <rohanpan27@gmail.com>
2022-02-08 16:09:01 +0530

    added comments

--- Primes.py
+++ Primes.py
@@ -1,21 +1,20 @@
 
 
-def prime(n):
+def prime(n)->list:
 	primes = []
-	for i in range(2, n+1):
-		if is_prime(i):
-			primes.append(i)
+
+	for i in range(2, n+1):						#Iterating from 2 -> n+1
+		if is_prime(i):							#call back
+			primes.append(i)					#add to list if true else no
 	return primes
 
 
-def is_prime(i):
-	for j in range(2, int(i**.5)+1):
-			if i%j == 0:
-				return False
+def is_prime(i)->bool:
+	for j in range(2, int(i**.5)+1):			#Since we can iterate all the way to 'i', why waste time instead to i^0.5
+			if i%j == 0:						#if divisible, then 'i' is not prime
+				return False 					
 	return True
 
 
-sum_of_all_primes = lambda n: sum(i for i in range(2,n+1) if is_prime(i))
-is_prime = lambda i: all(i%j for j in range(2,int(i**.5)+1))
-
-print(prime(20))
+prime = lambda n: [i for i in range(2,n+1) if is_prime(i)]		#code golf variation
+is_prime = lambda i: all(i%j for j in range(2,int(i**.5)+1))	#code golf variation
