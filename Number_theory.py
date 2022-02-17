d13e22d704545fb1e26e44d120869d3aaa637025
N0R4H <rohanpan27@gmail.com>
2022-02-17 09:04:29 +0530

    number theory q2 added

--- Number_theory.py
+++ Number_theory.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict as d
 
 '''
 
@@ -17,7 +17,30 @@ def trailing_zeros(n):
 
 '''
 
-Q2: Find power of a non-prime in a factorial
+Q2: Find the maximum power of a number in a factorial
 TBD
 '''
 
+def maxpow(factorial, n):
+
+	'''
+	Decompose n into primes
+	'''
+	c = d(int)
+	for i in range(2,n+1):
+		if n == 1:
+			print(c)
+			for i in c:
+				exp = 0
+				f = factorial
+				while f>=1:
+					f//=i
+					exp+=f
+				c[i]=exp
+			return c
+		elif n%i == 0:
+			while n%i == 0:
+				c[i]+=1
+				n//=i
+
+print(maxpow(14,4))
